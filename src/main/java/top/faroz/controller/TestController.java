36f5bd2570ce5b5d4ba23ccb5f5f205ce6e0ff22
wcgzgj <zhangjia_good@126.com>
2021-03-20 14:25:13 +0800

    增加自定义配置项示例

--- src/main/java/top/faroz/controller/TestController.java
+++ src/main/java/top/faroz/controller/TestController.java
@@ -1,5 +1,6 @@
 package top.faroz.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+
+    //从配置文件中，读取信息  :后面是默认值，如果配置文件中没有，会去使用
+    @Value("${test.hello:TEST}")
+    private String data;
+
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Springboot";
+        return data;
     }
 
 }
