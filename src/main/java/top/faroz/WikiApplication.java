dabcc5e114892bb8656470f3c554082296f2bfd5
wcgzgj <zhangjia_good@126.com>
2021-03-20 13:15:43 +0800

    项目初始化

---
+++ src/main/java/top/faroz/WikiApplication.java
@@ -0,0 +1,13 @@
+package top.faroz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/faroz/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.faroz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
