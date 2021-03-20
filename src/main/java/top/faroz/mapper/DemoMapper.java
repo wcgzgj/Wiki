f684665b8c87f8f0bbd773674aacbb2f423a4f1e
wcgzgj <zhangjia_good@126.com>
2021-03-20 17:38:37 +0800

    集成mybatis-generator，生成demo表代码

---
+++ src/main/java/top/faroz/mapper/DemoMapper.java
@@ -0,0 +1,31 @@
+package top.faroz.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import top.faroz.pojo.Demo;
+import top.faroz.pojo.DemoExample;
+
+import java.util.List;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    Demo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
+}
