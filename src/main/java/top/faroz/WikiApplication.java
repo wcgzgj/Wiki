16957cad5aef0dfb628540d8bb24063b7e140c9a
wcgzgj <zhangjia_good@126.com>
2021-03-20 13:50:50 +0800

    修改启动文案

--- src/main/java/top/faroz/WikiApplication.java
+++ src/main/java/top/faroz/WikiApplication.java
@@ -1,14 +1,22 @@
 package top.faroz;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class WikiApplication {
 
+    private static final Logger LOG= LoggerFactory.getLogger(WikiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        ConfigurableEnvironment environment = app.run(args).getEnvironment();
+        LOG.info("启动成功");
+        LOG.info("地址:\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
     }
 
 }
