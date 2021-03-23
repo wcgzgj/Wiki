a9f9c31feded82909ca3e97cecd2afa4fc844022
wcgzgj <zhangjia_good@126.com>
2021-03-23 16:28:35 +0800

    完成首页布局，这是稳定版本，以后如果出现问题，回溯到这里即可

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <a-layout>
+        <the-header></the-header>
+        <router-view/>
+        <the-footer></the-footer>
+    </a-layout>
 </template>
 
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import TheFooter from './components/the-footer.vue'
+  import TheHeader from "./components/the-header.vue";
+
+  export default defineComponent({
+    name: 'Home',
+    components: {
+      TheHeader,
+      TheFooter
+    },
+  });
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #components-layout-demo-top-side-2 .logo {
+    float: left;
+    width: 120px;
+    height: 31px;
+    margin: 16px 24px 16px 0;
+    background: rgba(255, 255, 255, 0.3);
+  }
 
-#nav {
-  padding: 30px;
-}
+  .ant-row-rtl #components-layout-demo-top-side-2 .logo {
+    float: right;
+    margin: 16px 0 16px 24px;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .site-layout-background {
+    background: #fff;
+  }
 </style>
---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        FARO_Z 电子书
+    </a-layout-footer>
+</template>
+
+<script>
+    export default {
+        name: "the-footer"
+    }
+</script>
+
+<style scoped>
+
+</style>
