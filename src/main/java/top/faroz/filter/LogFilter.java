f432d2f4eccc0b123aefefb394aa531233a731c3
wcgzgj <zhangjia_good@126.com>
2021-03-23 19:21:44 +0800

    增加过滤器，打印接口耗时

---
+++ src/main/java/top/faroz/filter/LogFilter.java
@@ -0,0 +1,39 @@
+package top.faroz.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @ClassName LogFilter
+ * @Description 增加过滤器，记录接口访问时间
+ * @Author FARO_Z
+ * @Date 2021/3/23 下午7:18
+ * @Version 1.0
+ **/
+@Component
+public class LogFilter implements Filter{
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // 打印请求信息
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("------------- LogFilter 开始 -------------");
+        LOG.info("请求地址: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("远程地址: {}", request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+        LOG.info("------------- LogFilter 结束 耗时：{} ms -------------", System.currentTimeMillis() - startTime);
+    }
+}
