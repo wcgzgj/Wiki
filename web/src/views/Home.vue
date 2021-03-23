179f1bd1fee7a5ba32fa568a15f474fd685ede17
wcgzgj <zhangjia_good@126.com>
2021-03-23 17:32:21 +0800

    电子书布局调整，一行显示三个电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -49,35 +49,22 @@
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
 
-      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-        <template #footer>
-          <div>
-            <b>ant design vue</b>
-            footer part
-          </div>
-        </template>
+      <a-list item-layout="vertical" size="large" :data-source="ebooks" :grid="{ gutter: 20, column: 3 }">
+        <!--下面会进行循环，每次都会取出ebooks中的一个数据 item-->
         <template #renderItem="{ item }">
-          <a-list-item key="item.title">
+          <a-list-item key="item.name">
             <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
-            </template>
-            <template #extra>
-              <img
-                      width="272"
-                      alt="logo"
-                      src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
             </template>
             <a-list-item-meta :description="item.description">
               <template #title>
-                <a :href="item.href">{{ item.title }}</a>
+                <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.avatar" /></template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
             </a-list-item-meta>
-            {{ item.content }}
           </a-list-item>
         </template>
       </a-list>
@@ -92,19 +79,8 @@
   import axios from "axios";
 
 
-  const listData: Record<string, string>[] = [];
 
-  for (let i = 0; i < 23; i++) {
-    listData.push({
-      href: 'https://www.antdv.com/',
-      title: `ant design vue part ${i}`,
-      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-      description:
-              'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-      content:
-              'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-    });
-  }
+
 
 export default defineComponent({
   name: 'Home',
@@ -128,17 +104,20 @@ export default defineComponent({
       { type: 'MessageOutlined', text: '2' },
     ];
 
-    const data=ref();
+    const ebooks=ref();
+
     onMounted(()=>{
       console.log("onMounted");
       axios.get("http://localhost:8880/ebook/list?name=Spring").then(resp=>{
-        console.log(resp);
-        data.value=resp.data;
+        // data是http的数据部分
+        const data=resp.data;
+        // content是我们自定义的CommonResp中的内容部分
+        ebooks.value=data.content;
       })
     });
+
     return {
-      data,
-      listData,
+      ebooks,
       pagination,
       actions
     }
