139a62217bfbfe36b93b5ece21510fd86114ba7a
wcgzgj <zhangjia_good@126.com>
2021-03-23 13:56:24 +0800

    尝试添加eslint，如果失败，就返回

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -58,10 +58,31 @@
     <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-{{ebooks}}
-      </pre>
-      <a-button type="primary">hello world</a-button>
+
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <div slot="footer"><b>ant design vue</b> footer part</div>
+        <a-list-item slot="renderItem" key="item.title" slot-scope="item, index">
+<!--          <template v-for="{ type, text } in actions" slot="actions">-->
+<!--            <span :key="type">-->
+<!--              <a-icon :type="type" style="margin-right: 8px" />-->
+<!--              {{ text }}-->
+<!--            </span>-->
+<!--          </template>-->
+          <img
+                  slot="extra"
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+          />
+          <a-list-item-meta :description="item.description">
+            <a slot="title" :href="item.href">{{ item.title }}</a>
+            <a-avatar slot="avatar" :src="item.avatar" />
+          </a-list-item-meta>
+          {{ item.content }}
+        </a-list-item>
+      </a-list>
+
+
     </a-layout-content>
   </a-layout>
 
@@ -71,6 +92,20 @@
 <script lang="ts">
   import { defineComponent ,onMounted , ref} from 'vue';
   import axios from 'axios'
+
+
+  const listData: any = [];
+  for (let i = 0; i < 23; i++) {
+    listData.push({
+      href: 'https://www.antdv.com/',
+      title: `ant design vue part ${i}`,
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      description:
+              'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+      content:
+              'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+    });
+  }
 
 export default defineComponent({
   name: 'Home',
@@ -91,9 +126,22 @@ export default defineComponent({
       })
     })
 
+
     return {
       //最后要把响应式数据ebooks返回，前端才可以拿到
-      ebooks
+      ebooks,
+      listData,
+      pagination: {
+        onChange: (page: any) => {
+          console.log(page);
+        },
+        pageSize: 3,
+      },
+      actions: [
+        { type: 'star-o', text: '156' },
+        { type: 'like-o', text: '156' },
+        { type: 'message', text: '2' },
+      ],
     }
   }
 
