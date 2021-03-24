330aab82dc9007759b3384f4f7cd9fd9c57e79b6
wcgzgj <zhangjia_good@126.com>
2021-03-24 13:49:44 +0800

    修改电子书管理布局

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        电子书管理
-    </div>
+    <a-layout>
+        <a-layout-content
+                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+            <div>
+                电子书管理
+            </div>
+        </a-layout-content>
+    </a-layout>
 </template>
 
 <script>
