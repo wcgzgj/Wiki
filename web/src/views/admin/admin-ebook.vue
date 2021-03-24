f6160c5a94fd6b68b4915547934a24a1eeda708d
wcgzgj <zhangjia_good@126.com>
2021-03-24 16:22:55 +0800

    模态框显示电子书表单，编辑某一行时，可以将改行数据显示到表单

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -14,9 +14,9 @@
                 <template #cover="{ text: cover }">
                     <img v-if="cover" :src="cover" alt="avatar" />
                 </template>
-                <template v-slot:action="{}">
+                <template v-slot:action="{record}">
                     <a-space size="small">
-                        <a-button type="primary" @click="edit">
+                        <a-button type="primary" @click="edit(record)">
                             编辑
                         </a-button>
                         <a-button type="danger">
@@ -34,7 +34,23 @@
             :confirm-loading="modalLoading"
             @ok="handleModalOk"
     >
-        <p>测试电子书表单</p>
+        <a-form :model="ebook" :label-col="{ span: 6 }" :wrapper-col="{ span: 18 }">
+            <a-form-item label="封面">
+                <a-input v-model:value="ebook.cover" />
+            </a-form-item>
+            <a-form-item label="名称">
+                <a-input v-model:value="ebook.name" />
+            </a-form-item>
+            <a-form-item label="分类一">
+                <a-input v-model:value="ebook.category1Id" />
+            </a-form-item>
+            <a-form-item label="分类二">
+                <a-input v-model:value="ebook.category2Id" />
+            </a-form-item>
+            <a-form-item label="描述">
+                <a-input v-model:value="ebook.desc" type="textarea" />
+            </a-form-item>
+        </a-form>
     </a-modal>
 
 </template>
@@ -133,6 +149,7 @@
             };
 
             // -------- 表单 ---------
+            const ebook=ref({});
             const modalVisible = ref(false);
             const modalLoading = ref(false);
             const handleModalOk = () => {
@@ -145,9 +162,15 @@
 
             /**
              * 编辑
+             * 这里的const xxx=()=>{}
+             * 其实就是一个函数
+             * 函数名为 xxx
+             * 实现为()=>{}
+             * ()里面可以放参数
              */
-            const edit = () => {
+            const edit = (record) => {
                 modalVisible.value = true;
+                ebook.value=record;
             };
 
 
@@ -167,6 +190,7 @@
 
                 edit,
 
+                ebook,
                 modalVisible,
                 modalLoading,
                 handleModalOk
