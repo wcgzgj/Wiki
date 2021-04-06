8b13e5eb6b6292a87e56db3104e47c7428bf3be3
wcgzgj <zhangjia_good@126.com>
2021-04-06 17:52:31 +0800

    文档内容表设计与代码生成

---
+++ src/main/java/top/faroz/mapper/ContentMapper.java
@@ -0,0 +1,35 @@
+package top.faroz.mapper;
+
+import org.apache.ibatis.annotations.Param;
+import top.faroz.pojo.Content;
+import top.faroz.pojo.ContentExample;
+
+import java.util.List;
+
+public interface ContentMapper {
+    long countByExample(ContentExample example);
+
+    int deleteByExample(ContentExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Content record);
+
+    int insertSelective(Content record);
+
+    List<Content> selectByExampleWithBLOBs(ContentExample example);
+
+    List<Content> selectByExample(ContentExample example);
+
+    Content selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByExample(@Param("record") Content record, @Param("example") ContentExample example);
+
+    int updateByPrimaryKeySelective(Content record);
+
+    int updateByPrimaryKeyWithBLOBs(Content record);
+}
