3554a538e9369ac0744f97fd1951f0766a55cea8
wcgzgj <zhangjia_good@126.com>
2021-04-10 23:52:18 +0800

    集成WebSocket

---
+++ src/main/java/top/faroz/config/WebSocketConfig.java
@@ -0,0 +1,20 @@
+package top.faroz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @ClassName WebSocketConfig
+ * @Description TODO
+ * @Author FARO_Z
+ * @Date 2021/4/10 下午2:55
+ * @Version 1.0
+ **/
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
