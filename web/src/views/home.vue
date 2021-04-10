1a497eea248e197cbba34a4daf31786ae0a284cc
wcgzgj <zhangjia_good@126.com>
2021-04-10 14:45:06 +0800

    首页显示电子书信息：文档数、阅读数、点赞数 doc/vote/** 接口不需要登录拦截

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -38,9 +38,22 @@
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
-              <span v-for="{ type, text } in actions" :key="type">
-                <component v-bind:is="type" style="margin-right: 8px" />
-                {{ text }}
+              <!--<span v-for="{ type, text } in actions" :key="type">-->
+              <!--  <component v-bind:is="type" style="margin-right: 8px" />-->
+              <!--  {{ text }}-->
+              <!--</span>-->
+
+              <span>
+                <component v-bind:is="'FileOutlined'" style="margin-right: 8px" />
+                {{ item.docCount }}
+              </span>
+              <span>
+                <component v-bind:is="'UserOutlined'" style="margin-right: 8px" />
+                {{ item.viewCount }}
+              </span>
+              <span>
+                <component v-bind:is="'LikeOutlined'" style="margin-right: 8px" />
+                {{ item.voteCount }}
               </span>
             </template>
             <a-list-item-meta :description="item.description">
@@ -86,11 +99,11 @@
       },
       pageSize: 3,
     };
-    const actions: Record<string, string>[] = [
-      { type: 'StarOutlined', text: '156' },
-      { type: 'LikeOutlined', text: '156' },
-      { type: 'MessageOutlined', text: '2' },
-    ];
+    // const actions: Record<string, string>[] = [
+    //   { type: 'StarOutlined', text: '156' },
+    //   { type: 'LikeOutlined', text: '156' },
+    //   { type: 'MessageOutlined', text: '2' },
+    // ];
 
     const ebooks=ref();
 
@@ -154,7 +167,6 @@
     return {
       ebooks,
       pagination,
-      actions,
       handleClick,
       level1,
       isShowWelcome
