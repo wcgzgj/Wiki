74d829d823a0538d2211da14e072ca0c02fc0c9d
wcgzgj <zhangjia_good@126.com>
2021-04-10 14:37:23 +0800

    增加定时器，定时执行电子书信息更新SQL，增加日志

--- src/main/java/top/faroz/job/DocJob.java
+++ src/main/java/top/faroz/job/DocJob.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import top.faroz.service.DocService;
 
+import javax.annotation.Resource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -19,17 +21,23 @@ import java.util.Date;
 public class DocJob {
     private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
 
+    @Resource
+    DocService docService;
+
 
     /**
      * 自定义cron表达式跑批
      * 只有等上一次执行完成，下一次才会在下一个时间点执行，错过就错过
      * cron 表达式不用记，可以在这个网址生成：https://cron.qqe2.com/
-     *
+     * <p>
      * 下面这段表示：每隔 30 秒，更新一次电子书信息
      */
     @Scheduled(cron = "1/30 * * * * ? ")
     public void cron() throws InterruptedException {
-        SimpleDateFormat formatter = new SimpleDateFormat("mm:ss SSS");
-        String dateString = formatter.format(new Date());
+
+        LOG.info("更新电子书下的文档数据开始");
+        long start = System.currentTimeMillis();
+        docService.updateDocInfo();
+        LOG.info("更新电子书下的文档数据结束，耗时：{}毫秒", System.currentTimeMillis() - start);
     }
 }
